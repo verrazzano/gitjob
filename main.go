877f679633d21e0034c1f3fd51b4840d536eecba
Jonathan Maron <jonathan.maron@oracle.com>
2023-05-04 16:21:24 -0400

    add env printout

--- main.go
+++ main.go
@@ -63,6 +63,7 @@ func main() {
 
 func run(c *cli.Context) {
 	logrus.Info("Starting controller")
+	logrus.Infof("Environment: %v", os.Environ())
 	ctx := signals.SetupSignalHandler(context.Background())
 
 	if c.Bool("debug") {
